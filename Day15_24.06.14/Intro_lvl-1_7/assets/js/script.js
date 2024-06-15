e816c9829d0b8a21215a27a76ac795485ebf7cd0
marcusslomka <marcus.slomka@googlemail.com>
2024-06-15 17:41:47 +0200

    done

---
+++ Day15_24.06.14/Intro_lvl-1_7/assets/js/script.js
@@ -0,0 +1,20 @@
+let helloWorld = "Hello World"
+console.log(helloWorld)
+
+const pi = 3.14
+console.log(pi)
+
+let burjDubai = "828m"
+console.log(burjDubai)
+
+let fullName = "Jan Schmidt"
+console.log(fullName)
+
+const eifeltower = "324m"
+console.log(eifeltower)
+
+let camelCase = "camelCase"
+console.log(camelCase)
+
+let kursStatus = true
+console.log (kursStatus)
