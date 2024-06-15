1896556aac4d481fc57b5c45c99a9206751c44db
marcusslomka <marcus.slomka@googlemail.com>
2024-06-15 17:57:03 +0200

    done

---
+++ Day15_24.06.14/Console-lvl-1_1/assets/js/sript.js
@@ -0,0 +1,20 @@
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+let firstName = "Anton";
+console.log(firstName)
+
+let age = 28;
+console.log(age);
+
+let job = "Trainer:in";
+console.log(job);
+
+let married = true;
+console.log (married)
+
+console.log (firstName + " ist " + age + " Jahre alt, von Beruf " + job + " und ist verheiratet:" + married)
