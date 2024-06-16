cd574daeeaea5adf4abbade2fdb35b2a92634cbf
marcusslomka <marcus.slomka@googlemail.com>
2024-06-16 12:34:31 +0200

    done

---
+++ Day15_24.06.14/intro-lvl-1_3/assets/js/script.js
@@ -0,0 +1,17 @@
+let i = 1;
+console.log(typeof (i));
+
+let lastName = "johnson";
+console.log(typeof (lastName));
+
+let j = 2;
+console.log(typeof (j));
+
+let status = true;
+console.log(typeof (status));
+
+let hello = "hello";
+console.log(typeof (hello));
+
+let helloWorld = "hello"
+console.log (typeof (helloWorld));    
