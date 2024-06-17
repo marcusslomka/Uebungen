8b55c3941f5ecb800f5ee25cd16b1843b96e8dc0
marcusslomka <marcus.slomka@googlemail.com>
2024-06-17 14:19:03 +0200

    Aufgabe 4 done

--- Day16_24.06.17/function-lvl-1_2/assets/js/string.js
+++ Day16_24.06.17/function-lvl-1_2/assets/js/string.js
@@ -1,3 +1,4 @@
+//! Aufgabe 2
 function intro2(paramName) {
     let name = "SuperCode:in";
     console.log(`Hi ${name}. Mein Name ist ${paramName}`);
@@ -7,9 +8,24 @@ intro2("Achim");
 intro2("Utrit");
 intro2("Ligma");
 
+//! Aufgabe 3
 function intro3(name,stadt,alter) {
     console.log(`Hallo, mein Name ist ${name}, ich bin ${alter} alt und komme aus ${stadt}.`);
 }   
 intro3("Marcus", "Berlin", 27);
 intro3("Jan", "Flensburg", 26);
 intro3("Mclovin", "Hawaii", 25);
+
+//!Aufgabe 4
+function math(a, b) {
+    
+    console.log(`${a} * ${b} = ${a * b}`);
+    console.log(`${a} / ${b} = ${a / b}`);
+}
+
+math(10, 2);
+math(100, 100);
+math(5,0);
+math(45, 173);
+math(1, 1000);
+
