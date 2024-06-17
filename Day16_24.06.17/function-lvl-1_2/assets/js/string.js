ac597f864cbf7cadff444f021e4e7f3ea6da1749
marcusslomka <marcus.slomka@googlemail.com>
2024-06-17 14:08:34 +0200

    3.Aufgabe done

--- Day16_24.06.17/function-lvl-1_2/assets/js/string.js
+++ Day16_24.06.17/function-lvl-1_2/assets/js/string.js
@@ -6,3 +6,10 @@ function intro2(paramName) {
 intro2("Achim");
 intro2("Utrit");
 intro2("Ligma");
+
+function intro3(name,stadt,alter) {
+    console.log(`Hallo, mein Name ist ${name}, ich bin ${alter} alt und komme aus ${stadt}.`);
+}   
+intro3("Marcus", "Berlin", 27);
+intro3("Jan", "Flensburg", 26);
+intro3("Mclovin", "Hawaii", 25);
